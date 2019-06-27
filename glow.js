9677f37d4f47404924a8fa2171455e4eccdde7b7
matt j <mattj@carmmunity.co.uk>
2019-06-27 16:26:09 +0100

    border glow

--- glow.js
+++ glow.js
@@ -9,6 +9,7 @@ var glow = {
 	glowRate: 100,
 	area: 'border',
 	originalBackground: '',
+	originalBorderColor: '',
 
 	setDiv(targetDiv) {
 		this.targetDiv = targetDiv;
@@ -39,15 +40,10 @@ var glow = {
 	   
 		switch(this.area) {
 			case "border":
-				document.getElementById(that.targetDiv).style.border = (that.i / 2) + "px solid #" + that.glowColour;
+				this.border();
 				break;
 
 			case "rainbow-background":
-
-				var nextDec = startDec - (decIncrement * that.i);
-				var hexString = "#"+nextDec.toString(16);
-				
-				document.getElementById(that.targetDiv).style.background = hexString;
 				break;
 
 			case 'background':
@@ -90,6 +86,7 @@ var glow = {
 
 	background:function() {
 		var that = this;
+
 		this.orignalBackground = document.getElementById(this.targetDiv).style.background;
 		var list = this.glowColour.match(/\d+/g)
 		this.interval = setInterval(function() {
@@ -97,11 +94,35 @@ var glow = {
 			document.getElementById(that.targetDiv).style.backgroundColor = 'rgb('+list[0]+', '+list[1]+', '+list[2]+', '+ ((1 / that.duration ) * that.i) +')';
 			that.glowTimer();
 
-
 		}, this.glowRate);
 
+	},
 
-		
+	// rainbowBackground:function() {
+
+	// 	var nextDec = startDec - (decIncrement * that.i);
+	// 	var hexString = "#"+nextDec.toString(16);
+				
+	// 	document.getElementById(that.targetDiv).style.background = hexString;
+
+	// 	this.interval = setInterval(function() {
+	// 		that.glowTimer();
+
+	// 	}, this.glowRate);
+	// }
+
+	border:function() {
+		var that = this;
+
+		this.originalBorderColor = document.getElementById(this.targetDiv).style.borderColor;
+		var list = this.glowColour.match(/\d+/g)
+
+		this.interval = setInterval(function() {
+
+			document.getElementById(that.targetDiv).style.borderColor = 'rgb('+list[0]+', '+list[1]+', '+list[2]+', '+ ((1 / that.duration ) * that.i) +')';
+			that.glowTimer();
+
+		}, this.glowRate);
 	},
 
 
@@ -113,6 +134,10 @@ var glow = {
 				
 				document.getElementById(this.targetDiv).style.background = this.orignalBackground;
 
+				break;
+			case 'border':
+
+				document.getElementById(this.targetDiv).style.borderColor = this.originalBorderColor;
 				break;
 		}
 	}
