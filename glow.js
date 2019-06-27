0225a5311bb8a160a695145f714deb8ecd016660
matt j <mattj@carmmunity.co.uk>
2019-06-27 15:26:46 +0100

    added background glow and example

--- glow.js
+++ glow.js
@@ -1,39 +1,120 @@
 var glow = {
-	
+	i:0,
+	duration:8,
+	interval:'',
+	stop:false,
+	reverse:false,
 	targetDiv: 'message',
 	glowColour: '#0000FF',
-	glowTime: 100,
-	
+	glowRate: 100,
+	area: 'border',
+	originalBackground: '',
+
+	setDiv(targetDiv) {
+		this.targetDiv = targetDiv;
+	},
+
+	setGlowColour(glowColour) {
+		this.glowColour = glowColour;
+	},
+
+	setGlowRate(glowRate) {
+		this.glowRate = glowRate;
+	},
+
+	setType(area) {
+		this.area = area;
+	},
+
+	setDuration(duration) {
+		this.duration = duration;
+	},
+
 	fire:function() {
-		var that = this;
-		var stop = false;
-		var reserse = false;
+	
+
+		// var startDec = parseInt('ffffff', 16);
+		// var endDec = parseInt(that.glowColour, 16);
+		// var decIncrement = (startDec - endDec)  / 4;
 	   
-		this.interval = setInterval(function() {
-			if(stop) {
-				that.stop();
-			}
+		switch(this.area) {
+			case "border":
+				document.getElementById(that.targetDiv).style.border = (that.i / 2) + "px solid #" + that.glowColour;
+				break;
 
-			if(that.i < 3 && !reserse) {
-				that.i++;
-			}
-			else {
-				reserse = true;
-				that.i--;
-				if(that.i == 0) {
-					stop = true;
-				}
-			}
+			case "rainbow-background":
 
-			document.getElementById(that.targetDiv).style.border =  (that.i / 2) + "px solid " + that.glowColour;
+				var nextDec = startDec - (decIncrement * that.i);
+				var hexString = "#"+nextDec.toString(16);
+				
+				document.getElementById(that.targetDiv).style.background = hexString;
+				break;
 
-		}, that.glowTime);
+			case 'background':
+				
+				this.background();
+
+				break;
+		}
 
 		return;
 	},
 
-	stop:function() {
+	glowTimer:function() {
+
+		//var that = this;
+		
+
+		if(this.stop) {
+			
+			this.stopInterval();
+			this.reverse = false;
+			this.stop = false;
+		}
+
+		else if(this.i < this.duration && !this.reverse) {
+			this.i++;
+			
+		}
+		else {
+			this.reverse = true;
+			this.i--;
+			if(this.i == 0) {
+				this.stop = true;
+			}
+		}
+
+		
+
+	}, 
+
+	background:function() {
+		var that = this;
+		this.orignalBackground = document.getElementById(this.targetDiv).style.background;
+		var list = this.glowColour.match(/\d+/g)
+		this.interval = setInterval(function() {
+
+			document.getElementById(that.targetDiv).style.backgroundColor = 'rgb('+list[0]+', '+list[1]+', '+list[2]+', '+ ((1 / that.duration ) * that.i) +')';
+			that.glowTimer();
+
+
+		}, this.glowRate);
+
+
+		
+	},
+
+
+	stopInterval:function() {
 		clearInterval(this.interval);
+
+		switch(this.area) {
+			case 'background':
+				
+				document.getElementById(this.targetDiv).style.background = this.orignalBackground;
+
+				break;
+		}
 	}
 	
 }
